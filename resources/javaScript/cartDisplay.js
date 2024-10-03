ce9a6241147560782a602fd89029cf8b1e4743bb
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-10-03 18:44:24 +0200

    Update cartDisplay.js

--- resources/javaScript/cartDisplay.js
+++ resources/javaScript/cartDisplay.js
@@ -1,4 +1,4 @@
-//required DOM elements
+//required variables to access the DOM
 const openCartBtns = document.querySelectorAll(".open-cart");
 const cartContainer = document.querySelector(".cart-container");
 const grandTotal = document.querySelector(".total-price");
