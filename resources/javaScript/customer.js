4490639470a82c87cf7ad6ea14b35bfc742c3954
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-12-17 16:06:13 +0200

    Update customer.js

--- resources/javaScript/customer.js
+++ resources/javaScript/customer.js
@@ -1,3 +1,4 @@
+//required elements to access the DOM
 const burgerMenu = document.querySelector(".hamburger-menu");
 const hamLinks = document.querySelector(".ham-links");
 const closeLinks = document.querySelector(".bx-x");
