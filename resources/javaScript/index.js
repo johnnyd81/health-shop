e6fdf30e8867af602a1ac7da1960e4ea1d07e703
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-12-17 16:06:47 +0200

    Update index.js

--- resources/javaScript/index.js
+++ resources/javaScript/index.js
@@ -1,4 +1,4 @@
-//required DOM elements
+//required elements to access the DOM
 const burgerMenu = document.querySelector(".hamburger-menu");
 const hamLinks = document.querySelector(".ham-links");
 const closeLinks = document.querySelector(".bx-x");
