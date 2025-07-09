8d6d261720565e9eb3b587cea7088fc52b11cc58
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-07-09 14:56:39 +0200

    Update customer.js

--- resources/javaScript/customer.js
+++ resources/javaScript/customer.js
@@ -8,7 +8,7 @@ burgerMenu.onclick = () => {
   hamLinks.classList.add("active"); //the add method is available on the classList object
 };
 
-//close the links by removing the active class from the hamLinks element
+//close the links by removing the "active" class from the hamLinks element
 closeLinks.onclick = () => {
   //use the remove method to remove the 'active' class from the classList object
   hamLinks.classList.remove("active");
